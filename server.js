2315e51fa3b99ac6fe86be24c27c3b3d5bc27df4
Sqrt of Saturn <sqrt@octoblu.com>
2015-07-07 10:26:34 -0700

    remove static

--- server.js
+++ server.js
@@ -77,8 +77,6 @@ app.use(bodyParser.urlencoded({ extended : true, limit : '50mb' }));
 
 app.use(bodyParser.json({ limit : '50mb' }));
 
-app.use(express.static(__dirname + '/public'));
-
 var meshbluJSON;
 try {
     meshbluJSON  = require(process.cwd()+'/meshblu.json');
