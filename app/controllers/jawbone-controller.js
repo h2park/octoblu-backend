103eb7d1e674ef8057e7394edb5be075694c1bc1
Square Root of Saturn <sqrt@octoblu.com>
2015-12-10 14:53:07 -0700

    v1.11.9 add heartrate_read to jawbone oauth

--- app/controllers/jawbone-controller.js
+++ app/controllers/jawbone-controller.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 
-var jawboneScope = ['basic_read', 'extended_read', 'location_read', 'friends_read', 'mood_read', 'mood_write', 'move_read', 'move_write', 'sleep_read', 'sleep_write', 'meal_read', 'meal_write', 'weight_read', 'weight_write', 'generic_event_read', 'generic_event_write'];
+var jawboneScope = ['basic_read', 'extended_read', 'location_read', 'friends_read', 'mood_read', 'mood_write', 'move_read', 'move_write', 'sleep_read', 'sleep_write', 'meal_read', 'meal_write', 'weight_read', 'weight_write', 'generic_event_read', 'generic_event_write', 'heartrate_read'];
 
 var JawboneController = function() {
     this.authorize = passport.authenticate('jawbone', {
