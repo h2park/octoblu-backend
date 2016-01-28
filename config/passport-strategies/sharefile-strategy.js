fdd6dd44a32d6f195d30d2974da7db03c8aeaa9a
Sqrt of Saturn <sqrt@octoblu.com>
2016-01-28 13:52:05 -0700

    v1.13.8 refresh token stuff for sharefile

--- config/passport-strategies/sharefile-strategy.js
+++ config/passport-strategies/sharefile-strategy.js
@@ -8,9 +8,10 @@ var CONFIG = Channel.syncFindOauthConfigByType('channel:sharefile');
 
 CONFIG.passReqToCallback = true;
 
-var sharefileStrategy = new ShareFileStrategy(CONFIG, function(req, accessToken, refreshToken, profile, done){
+var sharefileStrategy = new ShareFileStrategy(CONFIG, function(req, accessToken, refreshToken, params, profile, done){
+  var expiresOn = Date.now() + (params.expires_in * 1000);
 
-  User.addApiAuthorization(req.user, 'channel:sharefile', {authtype: 'oauth', token_crypt: textCrypt.encrypt(accessToken)}).then(function () {
+  User.addApiAuthorization(req.user, 'channel:sharefile', {authtype: 'oauth', token_crypt: textCrypt.encrypt(accessToken), refreshToken_crypt: textCrypt.encrypt(refreshToken), expiresOn: expiresOn}).then(function () {
     done(null, req.user);
   }).catch(function(error){
     done(error);
