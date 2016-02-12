2a29e31d8a661afd90166c32d795daa57afa0cc5
Square Root of Saturn <sqrt@octoblu.com>
2016-02-12 12:11:18 -0700

    v1.15.2 rename rightsignature so the icon isn't broken

--- config/passport-strategies/rightsignature-strategy.js
+++ config/passport-strategies/rightsignature-strategy.js
@@ -4,13 +4,13 @@ var User = require('../../app/models/user');
 var Channel = require('../../app/models/channel');
 var textCrypt    = require('../../app/lib/textCrypt');
 
-var CONFIG = Channel.syncFindOauthConfigByType('channel:rightSignature');
+var CONFIG = Channel.syncFindOauthConfigByType('channel:rightsignature');
 
 CONFIG.passReqToCallback = true;
 
 var rightsignatureStrategy = new RightSignatureStrategy(CONFIG, function(req, token, tokenSecret, profile, done) {
 
-  User.addApiAuthorization(req.user, 'channel:rightSignature', {
+  User.addApiAuthorization(req.user, 'channel:rightsignature', {
     authtype: 'oauth',
     token_crypt: textCrypt.encrypt(token),
     secret_crypt: textCrypt.encrypt(tokenSecret)
