d3d2f9d82500376f63efd40e0f3b70c51aa65afe
miql <mxander@gmail.com>
2016-02-17 15:04:46 -0700

    v1.15.5 Fix CLM cred encoding, better error-checking for auth

--- app/controllers/citrix-lifecycle-management-controller.js
+++ app/controllers/citrix-lifecycle-management-controller.js
@@ -10,60 +10,84 @@ var ClmController = function() {
 
   var self = this;
 
-  self.companies = function(req, originRes, servername, apiKey, next, callback){
-    var uri = 'https://' + servername + '/v0/companies?client_id=' + apiKey;
-    request.get({
-        url: uri,
-        timeout: 1500
-      }, function(err, httpResponse, body) {
-        body = JSON.parse(body);
-        if (err || !body[0].companyId) {
-          originRes.redirect('/home');
-          return;
-        }
-        callback(null, req, originRes, servername, body[0].companyId, apiKey, next);
+  self.companies = function(req, originRes, servername, apiKey, next, callback) {
+    var uri = 'https://' + servername + '/v0/companies?client_id=' + encodeURIComponent(apiKey);
+    console.log('get companies uri', uri);
+    var companiesOptions = {
+      url: uri,
+      timeout: 1500
+    }
+
+    request.get(companiesOptions, function(err, httpResponse, body) {
+      console.log('company body', body);
+      if (err || company) {
+        originRes.redirect('/home');
+        return;
+      }
+      try {
+        var companies = JSON.parse(body);
+        var company = companies[0].companyId;
+      } catch(err) {
+        callback(err);
+      }
+      callback(null, req, originRes, servername, company, apiKey, next);
     });
   };
 
   self.roles = function(req, originRes, servername, company_id, apiKey, next,callback){
-    var uri = 'https://' + servername + '/v0/roles?client_id=' + apiKey + '&company_id=' + company_id;
-    request.get({
-        url: uri
-      }, function(err, httpResponse, body) {
-        if (err || !body) {
-          originRes.redirect('/home');
-          return;
-        }
-        callback(null, req, originRes, JSON.parse(body)[0], company_id, next);
+    var uri = 'https://' + servername + '/v0/roles?client_id=' + encodeURIComponent(apiKey) + '&company_id=' + company_id;
+    var rolesOptions = {
+      url: uri
+    }
+
+    request.get(rolesOptions, function(err, httpResponse, body) {
+      if (err || !body) {
+        originRes.redirect('/home');
+        return;
+      }
+      try {
+        var roles = JSON.parse(body)[0];
+      } catch(err) {
+        callback(err);
+      }
+      callback(null, req, originRes, roles, company_id, next);
     });
   };
 
   self.finalize = function(req, res, role, company_id, next){
     var auth = 'Basic ' + new Buffer(req.query.apiKey + ':' + req.query.apiSecret, 'utf8').toString('base64');
     var uri = 'https://' + req.query.servername + '/v0/oauth/token';
-    var sc = role + ',' + company_id;
+    var scope = role + ',' + company_id;
 
-    request.get({
-        "url": uri,
-        "qs": {
-          "grant_type": 'client_credentials',
-          "scope": sc
-        },
-        "headers": {
-          "Authorization": auth
-        }
-      }, function(err, httpResponse, body) {
-        body = JSON.parse(body);
-        if (err || !body.access_token) {
-          res.redirect('/home');
-          return;
-        }
-        User.overwriteOrAddApiByChannelId(req.user, channelId, {authtype: 'oauth', token_crypt: textCrypt.encrypt(body.access_token)});
-        User.update({_id: req.user._id}, req.user).then(function(){
-          next();
-        }).catch(function(error){
-          next(error);
-        });
+    var finalizeOptions = {
+      "url": uri,
+      "qs": {
+        "grant_type": 'client_credentials',
+        "scope": scope
+      },
+      "headers": {
+        "Authorization": auth
+      }
+    };
+
+    request.get(finalizeOptions, function(err, httpResponse, body) {
+      if (err || !body.access_token) {
+        res.redirect('/home');
+        return;
+      }
+      try {
+        var token = JSON.parse(body);
+      } catch(err) {
+        callback(err);
+      }
+
+      var auth = {authtype: 'oauth', token_crypt: textCrypt.encrypt(token.access_token)}
+      User.overwriteOrAddApiByChannelId(req.user, channelId, auth);
+      User.update({_id: req.user._id}, req.user).then(function(){
+        next();
+      }).catch(function(error){
+        next(error);
+      });
     });
   };
 
