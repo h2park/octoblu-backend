12bfb49ea0913ca4c5c02063f041f2be864af921
Square Root of Saturn <sqrt@octoblu.com>
2016-02-25 10:18:29 -0700

    v1.17.1 remove console.log

--- app/controllers/channel-pagerduty-controller.js
+++ app/controllers/channel-pagerduty-controller.js
@@ -6,7 +6,6 @@ function ChannelPagerdutyController(){
 
   self.create = function(req, res){
     var token = "token=" + req.body.token;
-    console.log('I got token', token)
     User.overwriteOrAddApiByChannelId(req.user, req.params.id, { authtype: 'oauth', token_crypt : textCrypt.encrypt(token)});
     User.update({_id: req.user._id}, req.user).then(function(){
       res.send(201);
