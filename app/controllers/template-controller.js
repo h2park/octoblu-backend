e2e8f18641b51144f6a961b0a1e011558b6a49aa
alisamfp <eatliightning@gmail.com>
2016-03-23 17:24:13 -0700

    v1.19.9 add owner names when getting a single bluprint

--- app/controllers/template-controller.js
+++ app/controllers/template-controller.js
@@ -109,7 +109,7 @@ var TemplateController = function (options, dependencies) {
     return templateModel.findOne(req.uuid, query)
       .then(function(template) {
         req.template = template;
-        next();
+        self.addOwnerName(req, res, next);
       })
       .catch(function(error){
         res.send(422, error.message);
