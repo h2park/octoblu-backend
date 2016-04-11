3ea7398865c54de1e8d3c3316a1c5ef3abc98de7
sqrt <sqrt@octoblu.com>
2016-04-11 11:45:20 -0700

    v1.20.3 Fix XenMobile activation (probably, maybe?)

--- app/controllers/xenmobile-controller.js
+++ app/controllers/xenmobile-controller.js
@@ -2,12 +2,28 @@ var request = require('request');
 var Channel = require('../models/channel');
 var User = require('../models/user');
 var channelId = '56b390f3b604c4b06ed6de6a';
-// var applicationCredentials = Channel.syncFindById(channelId).applicationCredentials;
 var textCrypt = require('../lib/textCrypt');
 
 var XenMobileController = function(){
+  var cleanupAndRespondWithError = function(req, res, message){
+    console.error(message);
+
+    User.removeApiByChannelId(req.user, channelId).then(function(){
+      res.status(400).send({error: {message: message}});
+    }, function(){
+      res.send(500, {
+        error: {
+          message: 'Upstream Error, failed to cleanup channel activation',
+          upstreamError: {
+            message: message
+          }
+        }
+      });
+    });
+  };
+
   this.authorize = function(req, res, next){
-    var uri = 'https://' + req.query.serverUrl + '/xenmobile/api/v1/authentication/login';
+    var uri = req.query.serverUrl + '/xenmobile/api/v1/authentication/login';
     request.post({
         url: uri,
         json: {
@@ -15,11 +31,16 @@ var XenMobileController = function(){
           password: req.query.password
         }
       }, function(err, httpResponse, body) {
-        if (err || !body.auth_token) {
-          console.error('XenMobile Auth Failed:', err, body.auth_token);
-          res.redirect('/home');
-          return;
+        if (err) {
+          var message = 'XenMobile Auth Failed: ' + JSON.stringify(err.message);
+          return cleanupAndRespondWithError(req, res, message);
         }
+
+        if (!body.auth_token) {
+          var message = 'XenMobile Auth Failed: ' + JSON.stringify(body);
+          return cleanupAndRespondWithError(req, res, message);
+        }
+
         User.overwriteOrAddApiByChannelId(req.user, channelId, {authtype: 'header', token_crypt: textCrypt.encrypt(body.auth_token), headerParam: 'auth_token'});
         User.update({_id: req.user._id}, req.user).then(function(){
           next();
