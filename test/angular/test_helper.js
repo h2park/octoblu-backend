4a6c3885db35681f7950839ebb5dfa66f2629bda
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-04-26 13:37:12 -0700

    v1.21.3 remove angular tests

--- test/angular/test_helper.js
+++
@@ -1,8 +0,0 @@
-angular.module('octobluApp', ['ui.router']);
-window.expect = chai.expect;
-window.When = window.when
-window.meshblu = {
-  createConnection: function(){
-    return new EventEmitter();
-  }
-}
