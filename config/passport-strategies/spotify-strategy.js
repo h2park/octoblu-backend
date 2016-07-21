a86ddbdfc0d2d0de13385d19ae0f7a0be95405f1
erikwilson / iamruinous <Erik.E.Wilson@gmail.com , jade.meskill@gmail.com>
2016-07-20 17:16:50 -0700

    v2.2.2 support spotify refresh tokens

--- config/passport-strategies/spotify-strategy.js
+++ config/passport-strategies/spotify-strategy.js
@@ -9,8 +9,9 @@ var CONFIG = Channel.syncFindOauthConfigByType('channel:spotify');
 CONFIG.passReqToCallback = true;
 
 var spotifyStrategy = new SpotifyStrategy(CONFIG, function(req, accessToken, refreshToken, profile, done){
+  var expiresOn = Date.now() + (profile._json.push.expires_in * 1000);
 
-  User.addApiAuthorization(req.user, 'channel:spotify', {authtype: 'oauth', token_crypt: textCrypt.encrypt(accessToken)}).then(function () {
+  User.addApiAuthorization(req.user, 'channel:spotify', {authtype: 'oauth', token_crypt: textCrypt.encrypt(accessToken), refreshToken_crypt: textCrypt.encrypt(refreshToken), expiresOn: expiresOn}).then(function () {
     done(null, req.user);
   }).catch(function(error){
     done(error);
