19f8881c81c93aba45f1eef1c9ca24183637f075
Peter DeMartini <thepeterdemartini@gmail.com>
2017-01-04 09:12:46 -0700

    v4.2.0 added correct scopes to slack strategy

--- config/passport-strategies/slack-strategy.js
+++ config/passport-strategies/slack-strategy.js
@@ -9,14 +9,42 @@ var CONFIG = Channel.syncFindOauthConfigByType('channel:slack');
 CONFIG.scope = 'bot'
 CONFIG.passReqToCallback = true;
 CONFIG.scope = [
-  'identity.basic',
-  'identity.email',
-  'identity.avatar',
-  'identity.team',
+  'channels:history',
+  'channels:read',
+  'channels:write',
+  'chat:write:bot',
+  'chat:write:user',
+  'dnd:read',
+  'dnd:write',
+  'emoji:read',
+  'files:read',
+  'files:write:user',
+  'groups:history',
+  'groups:read',
+  'groups:write',
+  'im:history',
+  'im:read',
+  'im:write',
+  'mpim:history',
+  'mpim:read',
+  'mpim:write',
+  'pins:read',
+  'pins:write',
+  'reactions:read',
+  'reactions:write',
+  'search:read',
+  'stars:read',
+  'stars:write',
+  'team:read',
+  'usergroups:read',
+  'usergroups:write',
+  'users.profile:read',
+  'users.profile:write',
+  'users:read',
+  'users:write',
 ];
 
 var slackStrategy = new SlackStrategy(CONFIG, function(req, accessToken, refreshToken, profile, done){
-
   User.addApiAuthorization(req.user, 'channel:slack', {authtype: 'oauth', token_crypt: textCrypt.encrypt(accessToken)}).then(function () {
     done(null, req.user);
   }).catch(function(error){
